cc830da35857914d3e7eaa4a58407d42e0d9390f
datajoedata <116616136+datajoedata@users.noreply.github.com>
2023-06-20 22:59:00 -0300

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+filename = r'C:\Users\ninol\Desktop\FUNDO_NE\FNE_ALL_CLEAN.csv'
+output_filename = r'C:\Users\ninol\Desktop\FUNDO_NE\FNE_ALL_CLEAN.csv'
+
+df = pd.read_csv(filename, encoding='utf-8')
+
+unique_values = df['STATE'].unique()
+print("Unique values in 'STATE' column:")
+print(unique_values)
+
+# After printing unique values to have a first look, one thing I noticed was the incorrect number
+# of states, Brazilian northeast region only has 9 states. After some research in this matter, I've found that
+# the National Northeast Bank is
