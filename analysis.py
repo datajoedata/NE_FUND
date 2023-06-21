a35b9afefc23b95e7774e1da548e671038955a0d
datajoedata <116616136+datajoedata@users.noreply.github.com>
2023-06-21 17:01:12 -0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,11 +4,17 @@ filename = r'(filepath)
 
 df = pd.read_csv(filename, encoding='utf-8')
 
-unique_values = df['STATE'].unique()
-print("Unique values in 'STATE' column:")
-print(unique_values)
+# Iterate over each column in the DataFrame
 
+for column in df.columns:
+    unique_values = df[column].unique()
+    print(f"Unique values in '{column}' column:")
+    print(unique_values)
+    print()
+
+#////////////////////////
 # After using unique method to have a first look, one thing I've noticed was the incorrect number
 # of states, Brazilian northeast region only has 9 states. After some research in this matter, I've found that
 # the National Northeast Bank is subordinate to Federal Bank Instances. Even tho it's kind of weird finding
 # this values here, the respective Federal Bodies can lend loans in the name of FNE.
+
