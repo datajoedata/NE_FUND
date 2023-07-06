f0262b1605eed082a3fd3cd5d77d25d3c5c3c56f
datajoedata <116616136+datajoedata@users.noreply.github.com>
2023-07-05 23:00:57 -0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -38,5 +38,31 @@ print(df.columns)
 # this values here, Dunno if this was the case, but the respective Federal Bodies can lend loans in the name of FNE.
 
 
-# 3-
+# 3- Removing states and years that are out of our planned scope:
+
+excluded_states = ['MG', 'ES', 'RJ']   # ->>>> Are not part of NORTHEAST states
+df = df[~df['STATE'].isin(excluded_states)]
+
+exclude_years = [1996, 1999, 2020]     # ->>>> 1996 and 1999 we're not from the expected years, and 2020 will prob
+# give us outlying values due to covid
+
+df = df[~df['HIRE_DATE'].dt.year.isin(exclude_years)]
+
+# Calculating the avg. value of 'CONTRACT_VALUE' for each year:
+
+years = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011',
+         '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2021', '2022']
+
+state_list = df['STATE'].unique()
+
+# Create an empty DataFrame to store the average values for each year and state
+avg_values = pd.DataFrame(index=years, columns=state_list)
+
+for year in years:
+    df_year = df[df['HIRE_DATE'].dt.year == int(year)]
+    sum_year = df_year['CONTRACT_VALUE'].sum()
+    count_year = df_year.shape[0]
+    avg_year = sum_year / count_year
+    print(f"Average 'CONTRACT_VALUE' for year {year}: {avg_year}")
+
 
